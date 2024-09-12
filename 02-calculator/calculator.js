cda938901b0bbfd44d7e1e892504af5cf6981a16
Ankur-Ramoliya <131760623+Ankur-Ramoliya@users.noreply.github.com>
2024-09-12 22:56:21 +0530

    calculator foler add at home desktop

---
+++ 02-calculator/calculator.js
@@ -0,0 +1,7 @@
+function calculator() {
+    const numNine = document.querySelector('.btn-js-9');
+    numNine.addEventListener('click', function() {
+        document.querySelector('.screen-text').innerHTML = '9';
+    });
+}
+
