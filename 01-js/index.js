9baf5a35ee91ee87220058614cd225afd2c7039d
Ankur Ramoliya <ramoliya.ankur@gmail.com>
2024-09-13 19:01:01 +0530

    done this field input or blank is done

--- 01-js/index.js
+++ 01-js/index.js
@@ -9,16 +9,17 @@ function clicTObtn() {
     let fname = input1.value;
     let lname = input2.value;
         
-    if (fname === "", lname === "") {
-    
+    if (fname === "" || lname === "") {
+        document.querySelector('.para-element-js').innerHTML = "Input field is blank";
+    } else {
         document.querySelector('.para-element-js').innerHTML =  `Your Name: ` + fname + ' ' + lname;
         
         input1.value = "";
         input2.value = "";
         console.log(fname);
         console.log(lname);
-    } else {
-        document.querySelector(".para-element-js").innerHTML = alert(`Input field is blank`);
     }
         
 }; 
+
+
